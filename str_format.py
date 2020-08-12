e35d8f43de129872a321e38465ae366845ebf9c1
andreelezc <andreelezc@gmail.com>
2020-08-12 08:47:43 -0300

    first commit

---
+++ str_format.py
@@ -0,0 +1,13 @@
+age = 20
+name = 'Swaroop'
+
+print(f'{name} was {age} years old when he wrote this book')
+print(f'Why is {name} playing with that python?')
+
+# decimal (.) precision of 3 for float '0.333'
+print('{0:.3f}'.format(1.0/3))
+# fill with underscores (_) with the text centered
+# (^) to 11 width '___hello___'
+print('{0:_^11}'.format('hello'))
+# keyword-based 'Swaroop wrote A Byte of Python'
+print('{name} wrote {book}'.format(name='Swaroop', book='A Byte of Python'))
